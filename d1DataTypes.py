ab2213a203feed35ce7b430f0b1e3bd35e1e810c
Working Rom <dpriyeshvk@live.co.uk>
2016-05-29 01:25:42 +0530

    Create d1DataTypes.py

---
+++ d1DataTypes.py
@@ -0,0 +1,14 @@
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+myInt = int(input())
+myDouble = float(input())
+myStr = input()
+# Print the sum of both integer variables on a new line.
+print (i + myInt)
+print (d + myDouble)
+print (s + myStr)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
